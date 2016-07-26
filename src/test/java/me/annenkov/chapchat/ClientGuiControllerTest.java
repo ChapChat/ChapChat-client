8580043e05df494f1f0eed01063360e9d5b67823
ZZooRM <me@annenkov.me>
2016-07-26 22:20:53 +0300

    Created tests.

---
+++ src/test/java/me/annenkov/chapchat/ClientGuiControllerTest.java
@@ -0,0 +1,39 @@
+package me.annenkov.chapchat;
+
+public class ClientGuiControllerTest {
+    @org.junit.Test
+    public void run() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getSocketThread() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getUserName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getServerPort() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getServerAddress() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getModel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+
+}
